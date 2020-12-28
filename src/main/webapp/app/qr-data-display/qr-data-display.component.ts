cc255aa83db0e01b46acc2f05cad2b2ef8ecfa17
svenleonhard <sven.leonhard@7lofficial.de>
2020-12-28 17:30:33 +0100

    [main] url validation & download

--- src/main/webapp/app/qr-data-display/qr-data-display.component.ts
+++ src/main/webapp/app/qr-data-display/qr-data-display.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { NgxQrcodeElementTypes, NgxQrcodeErrorCorrectionLevels } from '@techiediaries/ngx-qrcode';
 import { UserQrCodeService } from 'app/entities/user-qr-code/user-qr-code.service';
 import { QrRouteService } from 'app/entities/qr-route/qr-route.service';
-import { QrRoute } from 'app/shared/model/qr-route.model';
+import { IQrRoute, QrRoute } from 'app/shared/model/qr-route.model';
 import { BASE_REDIRECTION_URL } from 'app/app.constants';
+import { NgForm, Validators } from '@angular/forms';
+import { doc } from 'prettier';
 
 @Component({
   selector: 'jhi-qr-data-display',
@@ -13,12 +15,16 @@ import { BASE_REDIRECTION_URL } from 'app/app.constants';
 export class QrDataDisplayComponent implements OnInit {
   elementType = NgxQrcodeElementTypes.URL;
   errorCorrectionLevel = NgxQrcodeErrorCorrectionLevels.MEDIUM;
-  value = 'https://google.de';
   margin = 0;
   scale = 11;
   redirectUrl = '';
   currentRedirection = this.redirectUrl;
   currentQrCode: QrRoute | null = null;
+  qrCodeImage = '';
+
+  urlPattern = '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?';
+
+  updateRedirect: any;
 
   constructor(private userQrCodeService: UserQrCodeService, private qrRouteService: QrRouteService) {}
 
@@ -43,5 +49,23 @@ export class QrDataDisplayComponent implements OnInit {
     });
   }
 
-  // updateRedirection() {}
+  updateRedirection(f: NgForm): void {
+    if (f.valid) {
+      if (this.updateRedirect && this.currentQrCode) {
+        this.currentQrCode.url = this.updateRedirect;
+        this.qrRouteService.update(this.currentQrCode).subscribe(res => {
+          if (res.body) {
+            this.currentQrCode = res.body;
+            if (this.currentQrCode && this.currentQrCode.url) {
+              this.currentRedirection = this.currentQrCode.url;
+            }
+          }
+        });
+      }
+    }
+  }
+
+  onDownload(): void {
+    this.qrCodeImage = document.getElementsByTagName('img')[0].src;
+  }
 }
--- src/main/webapp/app/qr-data-display/qr-data-display.module.ts
+++ src/main/webapp/app/qr-data-display/qr-data-display.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { QrDataDisplayComponent } from 'app/qr-data-display/qr-data-display.component';
 import { NgxQRCodeModule } from '@techiediaries/ngx-qrcode';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [QrDataDisplayComponent],
   exports: [QrDataDisplayComponent],
-  imports: [CommonModule, NgxQRCodeModule, RouterModule],
+  imports: [CommonModule, NgxQRCodeModule, RouterModule, FormsModule],
 })
 export class QrDataDisplayModule {}
